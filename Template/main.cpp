66f227b028a4c425a38e103a344eb482bb0d8cf1
AVAtarMod <Respawner289@gmail.com>
2023-10-29 18:27:42 +0300

    Template: Fix main file

--- Template/main.cpp
+++ Template/main.cpp
@@ -1,42 +1,39 @@
 #include "function.hpp"
-#include <filesystem>
 #include <iostream>
 #include <string>
 
-static const std::filesystem::path tablePath = "char_table.txt";
-
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
    bool noData = false;
-   Tokenizer t(tablePath);
-
-   if (argc == 1) {
-      std::cout << "Menu:\n"
-                << "\tPrint grammar table\n"
-                << "\tCheck input sequence in language\n"
-                << "Enter action number: ";
+   if (argc == 1)
+   {
+      std::cout << "Enter task number to run: ";
       argc++;
       noData = true;
    }
-   for (int i = 1; i < argc; i++) {
+   for (int i = 1; i < argc; i++)
+   {
       int choice;
-      if (noData) {
+      if (noData)
+      {
          std::cin >> choice;
          std::cin.ignore(32767, '\n');
-      } else {
+      }
+      else
+      {
          choice = std::stoi(argv[i]);
       }
 
-      switch (choice) {
-         case 1:
-            std::cout << "Table:\n";
-            std::cout << t.getTable() << '\n';
-            std::cout << "-----\n";
-            break;
-         default:
-            std::cerr << "ERROR: Incorrect action number (" << choice
-                      << ")!\n";
-            break;
+      switch (choice)
+      {
+      case 1:
+         std::cout << "Task #1\n";
+         task1();
+         std::cout << "-----\n";
+         break;
+      default:
+         std::cerr << "ERROR: Incorrect task number or task not exist\n";
+         break;
       }
    }
    return 0;
