a43f437c2fe4cb95ba170c5b40c2e00fa32fde34
AVAtarMod <Respawner289@gmail.com>
2023-10-29 21:48:56 +0300

    4LW: Update CMake project name, move parser to new project

--- 4LW/function.hpp
+++ 4LW/function.hpp
@@ -5,22 +5,11 @@
 #include <string>
 #include <vector>
 
+#include "lib_simplex_io/read_file.hpp"
+
 class SimplexMethodSolver
 {
   public:
-   enum MinMaxType
-   {
-      MIN,
-      MAX
-   };
-   enum ConstraintType
-   {
-      NONE,
-      LESSER_OR_EQUAL,
-      GREATER_OR_EQUAL,
-      EQUAL,
-   };
-
    SimplexMethodSolver(const std::filesystem::path& tablePath);
    std::string getSimplexTableuStr();
    std::string getProccessedFunction();
@@ -30,14 +19,15 @@ class SimplexMethodSolver
 
   private:
    typedef std::vector<std::pair<std::string, float>> function_repr;
-   typedef std::vector<std::pair<std::vector<float>, ConstraintType>>
+   typedef std::vector<
+     std::pair<std::vector<float>, lib_simplex_io::ConstraintType>>
      constraints_repr;
 
    function_repr input_function_;
    function_repr processed_function_;
    constraints_repr constraints_;
    std::vector<std::vector<float>> simplex_tableu_;
-   MinMaxType input_function_type_;
+   lib_simplex_io::MinMaxType input_function_type_;
 };
 
 #endif // FUNCTION_HPP
